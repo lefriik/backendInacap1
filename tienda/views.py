554c42b398d735d2415dd0d0dea464c48c8598e3
lefriik <emerio.isaac.suarez@gmail.com>
2022-09-13 11:19:34 -0300

    rutas card

--- tienda/views.py
+++ tienda/views.py
@@ -17,12 +17,12 @@ def index(request):
 def productos(request):
 
     productos = [
-        {'name': 'JUAN VALDEZ VOLCAN', 'precio': 10000, 'descripcion':'Café de sabor profundo', 'imagen': '/static/images/cafe1.webp'}, 
-        {'name': 'BIFTU GUDINA ETIOPIA', 'precio': 15000, 'descripcion':' Ejemplo de calidad', 'imagen': '/static/images/cafe2.webp'},
-        {'name': 'LUCCAFE GOLD EDITION', 'precio': 30000, 'descripcion':'Cafeína extra', 'imagen': '/static/images/cafe3.webp'},
-        {'name': 'MARLEY COFFEE', 'precio': 25000, 'descripcion':'One Love, One Heart', 'imagen': '/static/images/cafe4.webp'},
-        {'name': 'NESCAFÉ', 'precio': 18000, 'descripcion':'Fina Seleccion', 'imagen': '/static/images/cafe5.webp'},
-        {'name': 'Cruzeiro', 'precio': 22000, 'descripcion':'Tostado y Molido', 'imagen': '/static/images/cafe6.webp'}
+        {'name': 'JUAN VALDEZ VOLCAN', 'precio': 10000, 'descripcion':'Café de sabor profundo', 'imagen': 'cafe1.webp'}, 
+        {'name': 'BIFTU GUDINA ETIOPIA', 'precio': 15000, 'descripcion':' Ejemplo de calidad', 'imagen': 'cafe2.webp'},
+        {'name': 'LUCCAFE GOLD EDITION', 'precio': 30000, 'descripcion':'Cafeína extra', 'imagen': 'cafe3.webp'},
+        {'name': 'MARLEY COFFEE', 'precio': 25000, 'descripcion':'One Love, One Heart', 'imagen': 'cafe4.webp'},
+        {'name': 'NESCAFÉ', 'precio': 18000, 'descripcion':'Fina Seleccion', 'imagen': 'cafe5.webp'},
+        {'name': 'Cruzeiro', 'precio': 22000, 'descripcion':'Tostado y Molido', 'imagen': 'cafe6.webp'}
         
         ]
 
